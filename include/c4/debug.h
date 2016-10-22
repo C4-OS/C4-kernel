66249664cf83db26993bed62e661168e1fd644b1
My Name <foo@lisp.us.to>
2016-10-22 14:30:07 -0500

    Adding KASSERT macro for debugging

--- include/c4/debug.h
+++ include/c4/debug.h
@@ -2,6 +2,18 @@
 #define _C4_DEBUG_H 1
 #include <stdbool.h>
 
+#ifndef KNDEBUG
+#define KASSERT(CONDITION) { \
+		if ( !(CONDITION) ){ \
+			debug_printf( "%s:%u: assertion \"" #CONDITION "\" failed\n", \
+				__FILE__, __LINE__ ); \
+		} \
+	}
+
+#else
+#define KASSERT(CONDITION) /* CONDITION */
+#endif
+
 void debug_putchar( int c );
 void debug_puts( const char *str );
 void debug_printf( const char *format, ... );
